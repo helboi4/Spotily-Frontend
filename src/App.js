9cf7b6c66dfff3927c93c4b02bbeb090972212c2
helboi4 <h.hopecarter@gmail.com>
2021-11-29 11:32:24 +0000

    Initial commit. Created all react files, got rid of files that were not necessary and added all component and container file structure. Added routes in App.js to three pages. Imported imagesand icons into an images folder.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import UserPage from "./user/UserPage";
+import LoginPage from "./login/LoginPage";
+import AdminPage from "./admin/AdminPage";
+import {Routes, Route, Link} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path="/" component={LoginPage}/>
+        <Route exact path="/userpage" component={UserPage}/>
+        <Route exact path="/adminpage" component={AdminPage}/>
+      </Routes>
     </div>
   );
 }
