854eb31813865f092a5f63a6586c855abd0d5592
SabiDhiman <sabi98@live.co.uk>
2021-12-01 01:34:47 +0000

    added functionality to register button, added fetch api, need to check if post method works

--- src/LoginUI/Register.js
+++ src/LoginUI/Register.js
@@ -1,11 +1,42 @@
+import React, {useState} from 'react'
+import './Register.css'
+import {useHistory} from 'react-router-dom'
 function Register()
 {
-    return (
-        <div>
+    const [username, setUserName] =useState ("")
+    const [email, setEmail] = useState("")
+   // const [password, setPassword] = useState("")
+    const history =useHistory();
+
+    async function signUp() {
+        let parameters = {username, email}
+        console.warn(parameters)
+
+        let result = await fetch("http://localhost:8000/api/user/register",{
+            method: 'POST',
+            body:JSON.stringify(parameters),
+            headers:{
+                "Content-Type":'application/json',
+                "Accept":'application/json'
+        }
+    })
+    result = await result.json()
+    localStorage.setItem("user-info", JSON.stringify(result))
+    history.push("/add")
+}      
+    return(
+        <div className="container">
 <h1>Create Account</h1>
-<input type= "text" className="form-control" /> 
+<input type= "text" value={username} onChange ={(e)=>setUserName(e.target.value)} className="form-control" placeholder="please enter a username"/>
+<br /> 
+<input type= "text" value={email} onChange ={(e)=>setEmail(e.target.value)} className="form-control" placeholder="please enter an email"/> 
+<br /> 
+{/* <input type= "password" value={password} onChange ={(e)=>setPassword(e.target.value)}className="form-control" placeholder="please enter a password"/>  */}
+<br /> 
+<button onClick ={signUp} className="register-button">Register</button>
         </div>
     )
-}
 
-export default Register
+
+    }
+    export default Register
