d94ca8f5f6fc6ca2761622dad1cfda1c5bfa8a3d
helboi4 <h.hopecarter@gmail.com>
2021-12-01 19:59:45 +0000

    Rolled back to verison a20dc66 (two commits behind) and then turned the edit buttons into normal svg icons. We will no longer give them functionality

--- src/user/modals/SettingsModal.js
+++ src/user/modals/SettingsModal.js
@@ -24,11 +24,11 @@ const handleEmailChange = (event) => {
                 <form onSubmit={handleSubmit}>
                     <h2>Username:</h2>
                     <h3>{userName}</h3>
-                    <button type="button" className="edit-button"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><path d="M6 34.5v7.5h7.5l22.13-22.13-7.5-7.5-22.13 22.13zm35.41-20.41c.78-.78.78-2.05 0-2.83l-4.67-4.67c-.78-.78-2.05-.78-2.83 0l-3.66 3.66 7.5 7.5 3.66-3.66z"/><path fill="none" d="M0 0h48v48h-48z"/></svg></button>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><path d="M6 34.5v7.5h7.5l22.13-22.13-7.5-7.5-22.13 22.13zm35.41-20.41c.78-.78.78-2.05 0-2.83l-4.67-4.67c-.78-.78-2.05-.78-2.83 0l-3.66 3.66 7.5 7.5 3.66-3.66z"/><path fill="none" d="M0 0h48v48h-48z"/></svg>
                     <input type="text" onChange={handleNameChange} placeholder="Edit username"/>
                     <h2>Email Address:</h2>
                     <h3>{userEmail}</h3>
-                    <button type="button" className="edit-button"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><path d="M6 34.5v7.5h7.5l22.13-22.13-7.5-7.5-22.13 22.13zm35.41-20.41c.78-.78.78-2.05 0-2.83l-4.67-4.67c-.78-.78-2.05-.78-2.83 0l-3.66 3.66 7.5 7.5 3.66-3.66z"/><path fill="none" d="M0 0h48v48h-48z"/></svg></button>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><path d="M6 34.5v7.5h7.5l22.13-22.13-7.5-7.5-22.13 22.13zm35.41-20.41c.78-.78.78-2.05 0-2.83l-4.67-4.67c-.78-.78-2.05-.78-2.83 0l-3.66 3.66 7.5 7.5 3.66-3.66z"/><path fill="none" d="M0 0h48v48h-48z"/></svg>
                     <input type="text" onChange={handleEmailChange} placeholder="Edit email address"/>
                     <input type="submit"/>
                 </form>
